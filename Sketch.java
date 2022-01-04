7e8ab4131fad42c60f56dc77cfd6a21f46816ab1
John MacDonald <john.macdonald23@ycdsbk12.ca>
2022-01-03 19:35:38 -0500

    Java 5.4

--- Sketch.java
+++ Sketch.java
@@ -2,39 +2,77 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
-    public void settings() {
-        size(400, 400);
+
+  /**
+   * Called once at the beginning of execution, put your size all in this method
+   */
+  public void settings() {
+	  // put your size call here
+    size(500, 500);
+  }
+
+  /** 
+   * Called once at the beginning of execution.  Add initial set up
+   * values here i.e background, stroke, fill etc.
+   */
+
+  int intStillNum = 250;
+  double dblMoveAngle = 0.06;
+  int intScalar = 150;
+  double dblSpeed = 0.01;
+
+  int intMoonNum = 250;
+  double dblMoonAngle = 0.05;
+  int intMoonScalar = 150;
+
+  boolean Sun = true;
+  boolean Moon = false;
+
+  public void setup() {
+    background(210, 255, 173);
+  }
+
+  
+  public void draw() {
+    float SunHori = intStillNum + sin((float) dblMoveAngle) * intScalar;
+    float SunVert = intStillNum + cos((float) dblMoveAngle) * intScalar;
+
+    float MoonHori = intMoonNum + sin((float) dblMoonAngle) * intMoonScalar;
+    float MoonVert = intMoonNum + cos((float) dblMoonAngle) * intMoonScalar;
+
+    if (Sun == true){
+      background(39, 182, 214);
+    
+      fill(217, 171, 54);
+      ellipse(SunHori, SunVert, 75, 75);
+      dblMoveAngle = dblMoveAngle + dblSpeed;
+      
+      fill(52, 186, 61);
+      rect(0,350,500,500);
+
+      if (SunVert > 350){ 
+        Sun = false;
+        Moon = true;
+      }
+    }
+    
+    if (Moon == true){
+      background(0);
+      
+      fill(255,255,255);
+      ellipse(MoonHori, MoonVert, 75, 75);
+      dblMoonAngle = dblMoonAngle + dblSpeed;
+   
+      fill(52, 186, 61);
+      rect(0,350,500,500);
+
+      if (MoonVert > 350){
+        Sun = true;
+        Moon = false;
+      }
+
     }
 
-    public void setup() {
-        background(51, 204, 255);
-    }
 
-    public void draw() {
-
-        strokeWeight(4);
-        stroke(0, 0, 0);
-        fill(117, 73, 41);
-        triangle(width / 2 , height / 4, (float) (width / 1.06), (float) (height / 1.66), width / 16, (float) (height / 1.66));
-
-        strokeWeight(3);
-        fill(222, 158, 113);
-        rect(width / 16, (float) (height / 1.66), (float) (width/1.14), (float) (height / 1.05));
-
-        strokeWeight(2);
-        fill(60, 189, 222);
-        rect((float) (width / 6.66) , (float) (height / 1.6) , (float) (width / 5.3) , (float) (height / 5.3));
-
-        strokeWeight(2);
-        fill(117, 73, 41);
-        rect((float) (width / 2.66) , (float) (height / 1.45) , width / 4, (float) (height / 1.45));
-
-        fill(247, 237, 40);
-        ellipse((float) (width / 26.66) , height / 8, (float) (width / 5.33) , (float) (height / 4.44));
-
-        fill(102, 255, 102);
-        rect(0, (float) (height / 1.05) , width / 1, (float) (height / 1.33));
-
-
-    }
+  }  
 }
