5dc335e4a7cb716360299179ad62e12d5e83fcf3
John MacDonald <john.macdonald23@ycdsbk12.ca>
2022-01-13 12:23:46 -0500

    ignore this (cpt experiment)

--- Sketch.java
+++ Sketch.java
@@ -2,77 +2,47 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
+  boolean background = true;
 
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
 	  // put your size call here
     size(500, 500);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-
-  int intStillNum = 250;
-  double dblMoveAngle = 0.06;
-  int intScalar = 150;
-  double dblSpeed = 0.01;
-
-  int intMoonNum = 250;
-  double dblMoonAngle = 0.05;
-  int intMoonScalar = 150;
-
-  boolean Sun = true;
-  boolean Moon = false;
-
-  public void setup() {
-    background(210, 255, 173);
+  
+  public void draw() {   
+    updateBackground();
+    ellipse(100,100,400,400);
+    
+    rect(300,250,150,50);
+    rect(300,325,150,50);
+    rect(300,400,150,50);
   }
 
-  
-  public void draw() {
-    float SunHori = intStillNum + sin((float) dblMoveAngle) * intScalar;
-    float SunVert = intStillNum + cos((float) dblMoveAngle) * intScalar;
-
-    float MoonHori = intMoonNum + sin((float) dblMoonAngle) * intMoonScalar;
-    float MoonVert = intMoonNum + cos((float) dblMoonAngle) * intMoonScalar;
-
-    if (Sun == true){
-      background(39, 182, 214);
-    
-      fill(217, 171, 54);
-      ellipse(SunHori, SunVert, 75, 75);
-      dblMoveAngle = dblMoveAngle + dblSpeed;
-      
-      fill(52, 186, 61);
-      rect(0,350,500,500);
-
-      if (SunVert > 350){ 
-        Sun = false;
-        Moon = true;
-      }
-    }
-    
-    if (Moon == true){
-      background(0);
-      
-      fill(255,255,255);
-      ellipse(MoonHori, MoonVert, 75, 75);
-      dblMoonAngle = dblMoonAngle + dblSpeed;
    
-      fill(52, 186, 61);
-      rect(0,350,500,500);
 
-      if (MoonVert > 350){
-        Sun = true;
-        Moon = false;
-      }
-
+  public void updateBackground(){
+    if(background){
+      background(236, 171, 237);
     }
+    else{
+      background(255);
+    }
+  }
 
-
-  }  
+boolean isMouseOver(int x, int y, int w, int h){
+  if(mouseX >= x && mouseX <= x + w && mouseY >= y && mouseY <= y +h){
+    return true;
+  }
+  return false;
 }
+
+public void mousePressed(){
+  if(isMouseOver(100,100,400,400) == true){
+    background = !background;
+  }
+}
+}
+
+
+
